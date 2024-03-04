5b0e8021bea0f54fa30433c4fae12c533b2723ea
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 00:28:42 +0500

    Add files via upload

---
+++ expocalc.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const numOne= await inquirer.prompt({
+    name:"num1",
+    type:"number",
+    message:"Enter a number",
+})
+
+const numTwo= await inquirer.prompt({
+    name:"num2",
+    type:"number",
+    message:"Enter a second number",
+})
+
+let total= numOne.num1**numTwo.num2;
+console.log(total);
